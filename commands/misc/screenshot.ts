8ba397253812dd53c96f074f887de2389334c23e
Tenpi <pie4chan@gmail.com>
2025-02-18 16:09:50 -0500

    fixes

--- commands/misc/screenshot.ts
+++ commands/misc/screenshot.ts
@@ -63,9 +63,9 @@ export default class Screenshot extends Command {
         const website = (input.startsWith("http")) ? input.trim() : `https://${input.trim()}`
 
         const options = {
-          darkMode: true, delay: 1, overwrite: true, launchOptions: {args: ["--no-sandbox", "--disabled-setupid-sandbox"]},
-          width: 1280, height: 720, userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+          darkMode: true, delay: 1, overwrite: true, width: 1280, height: 720, userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
         } as any
+        if (process.platform === "linux") options.launchOptions = {executablePath: "/usr/bin/chromium-browser"}
         if (setMobile) options.emulateDevice = "iPhone XR"
 
         let dest = path.join(__dirname, `../../misc/images/dump/screenshot.png`)
